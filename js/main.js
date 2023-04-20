7bef6d1741513237e0e68d82b82e81eb4911b9c3
Marc Hudson <fiasst@gmail.com>
2023-04-20 16:29:27 +0100

    added onDomReady functions

--- js/main.js
+++ js/main.js
@@ -317,12 +317,294 @@ const MAIN = (function($, window, document, undefined){
 
 
     // On DOM ready.
-    $(function(){
-        //nav();
+    Webflow.push(function(){
+        // Get current Member.
+        window.$memberstackDom.getCurrentMember().then(({ data: member }) => {
+            window.MSmember = member || {};
+            //if (!data) {
+                //member is logged out
+            //}
+
+            //if (!!member.verified) {
+                //member has verified email.
+            //}
+            console.log(member);
+
+            if (pub.checkKeyExists(member, 'metaData')){
+                pub.replaceTextWithMetadata(member.metaData);
+            }
+
+            if (pub.checkKeyExists(member, 'id')){
+                // Add member ID to form field.
+                var hiddenInput = $('.input-member-id');
+
+                if (!!hiddenInput && !hiddenInput.val()){
+                    hiddenInput.val(member.id);
+                    hiddenInput.parents('form').find('.form-submit').removeAttr('disabled');
+                }
+            }
+
+            // Show content author controls (edit link...).
+            if (!!$('.node-author').length){
+                $('.node-author').each(function(){
+                    var authorID = $(this).attr('data-author'),
+                    display = (!!member && pub.checkKeyExists(member, 'id') && (member.id == authorID || pub.hasPermissions('can:moderate', member)));
+                    //console.log(member.id+' == '+authorID+' || '+hasPermissions('can:moderate', member));
+                    pub.controlHTML($(this).parents('.node').find('.author-access'), display);
+                });
+            }
+
+
+            // Show content if User has permissions.
+            $('[data-ms-perm]').each(function(){
+                pub.controlHTML($(this), pub.hasPermissions($(this).attr('data-ms-perm'), member));
+            });
+
+
+            /*
+            * Removed this for now because Weglot can't translate Thai to English, only the other way around...
+            * Could use Google Translate API for this feature instead.
+            *
+            $('.form-translate').on('click', function(){
+              var form = $('#' + $(this).attr('data-form')),
+                  clone = form.clone(),
+                  fields = [],
+                  strings = [];
+              
+              // Remove selects, buttons, passwords, tel/email fields and metadata from being translated.
+              clone.find(':input:not(.translate)').remove();
+              var fieldValues = getFormValues(clone);
+              
+              $.each(fieldValues, function(key, value){
+                if (!!value){
+                  strings.push({ 'w': value, 't': 2 });
+                  fields.push(key);
+                }
+              });
+              console.log(strings);
+              console.log(fields);
+              
+              Weglot.translate(
+                {
+                  'words': strings,
+                  'languageTo': 'th'
+                }, function(data){
+                  console.log('data', data);
+                  $.each(data, function(i, value){
+                    form.find('[name="'+ fields[i] +'"]').val(value);
+                    console.log(fields[i], value);
+                  });
+                }
+              );
+            });*/
+        });
+
+
+        // Delay forms.
+        //$('.delay-submit').one('submit', function(){
+        /*
+        * Doesn't work well with .ajax-submit submit handler. Fires AJAX twice...
+        *
+        $('.delay-submit').on('click.delaySubmit', '.delay-button', function(e){
+            console.log('delay-submit');
+            e.preventDefault();
+
+            let cookieFormValid = pub.getCookie('form-valid'),
+                form = $(this),
+                button = $(e.target),
+                d=20e3;
+
+            cookieFormValid = !!cookieFormValid?Number(cookieFormValid):d;
+            pub.buttonThinking(button);
+
+            setTimeout(function(){
+              form.off('.delaySubmit').submit();
+            }, cookieFormValid);
+            pub.setCookie('form-valid',cookieFormValid+d);
+
+            return false;
+        });*/
+
+        // AJAX forms.
+        $('.ajax-submit')
+            .on('click', '.form-submit', function(e){
+                $(e.target).addClass('clicked');
+            })
+            .on('submit', function(e){
+                e.preventDefault();
+
+                var form = $(this),
+                    button = form.find('.form-submit.clicked'),
+                    data = pub.getFormValues(form),
+                    formIncrement = pub.getCookie('form-valid'),
+                    i = 2;
+
+                formIncrement = !!formIncrement?Number(formIncrement):0;
+                data.increment = ++formIncrement;
+                pub.setCookie('form-valid',data.increment);
+
+                pub.buttonThinking(button);
+                console.log(data);
+
+                pub.sendAJAX({
+                    url: form.attr('action'),
+                    method: form.attr('method'),
+                    data: data,
+                    timeout: 120000,
+                    success: function(data){
+                        pub.handleResponse(data, form);
+                    },
+                    error: function(data){
+                        console.log('error');
+                    }
+                });
+            });
+
+
+        // Form fields: Populate form field's default values.
+        $('input[data-default-value]').each(function(){
+            if (!$(this).val()) {
+                $(this).val( $(this).attr('data-default-value') );
+            }
+        });
+
+
+        // Launch "Confirm" alert dialogs on element click.
+        $('.alert-confirm').on('click.alertConfirm', function(e){
+            var msg = $(this).attr('data-confirm');
+            if (msg){
+                e.preventDefault();
+              
+                if (confirm(msg)){
+                    $(this).off('click.alertConfirm').trigger('click');
+                }
+                else {
+                    // Remove a class that's added in another listener.
+                    $(this).removeClass('clicked');
+                    return false;
+                }
+            }
+        });
+
+
+        // Toggle element visibility.
+        $('.toggle-vis').on('click', function(e){
+            var target = $(this).attr('data-target');
+
+            if (target){
+                e.preventDefault();
+                $('#'+target).toggleClass('hide');
+            }
+        });
+
+
+        // Update form "op" (operation) value on button click.
+        // Because Webflow doesn't pass submit button values through to Make...
+        $('.form-submit[name="op"]').on('click', function(){
+            $(this).parents('form').find('.form-action-op').val( $(this).val() );
+        });
+
+
+        $('.node-job-row').each(function(){
+            var row = $(this),
+                actions = row.find('.table-links a'),
+                status = row.find('.col-status').text().toLowerCase();
+
+            if (!!status){
+                row.addClass(status);
+              
+                switch (status){
+                    case 'draft':
+                        // Show all links.
+                        actions.removeClass('hide');
+                        break;
+                    case 'expired':
+                    case 'archived':
+                        // Show all links.
+                        actions.removeClass('hide').filter('[data-link="publish"]').text('Republish');
+                        break;
+                    case 'pending':
+                    case 'published':
+                        // Show Edit link only.
+                        actions.filter('[data-link="edit"]').removeClass('hide');
+                }
+            }
+        });
+
+        // Publish Draft/Republish existing Job.
+        $('.link-hook-publish').on('click', function(e){
+            e.preventDefault();
+            var link = $(this);
+
+            if (link.hasClass('disabled')){
+                return false;
+            }
+            link.addClass('disabled');
+
+            var data = {
+                member_id: $(this).attr('data-member-id'),
+                item_id: $(this).attr('data-item-id'),
+                submitted: pub.getISOdate()
+            },
+            formIncrement = pub.getCookie('form-valid'),
+            i = 2;
+
+            formIncrement = !!formIncrement?Number(formIncrement):0;
+            data.increment = ++formIncrement;
+            pub.setCookie('form-valid',data.increment);
+
+            // Add thinking icon...
+
+            pub.sendAJAX({
+                url: "https://hook.us1.make.com/dv56t4535h1sfag5g0693924h3sg5582",
+                data: data,
+                timeout: 120000,
+                success: function(data){
+                    //pub.handleResponse(data);
+                },
+                error: function(data){
+                    console.log('error');
+                }
+            });
+        });
+
+
+        // Form fields: Populate select lists with Collection options.
+        $('.select-list-options').each(function(){
+            var wrapper = $(this).parent('.select-list-wrapper'),
+                select = wrapper.find('select'),
+                defaultValue = wrapper.find('.select-list-default-value').attr('data-value') || '';
+
+            $(this).find('.w-dyn-item').each(function(){
+                $(this).data('lang-en', $(this).text());// Store a non-translated string in .data().
+
+                $('<option />', {
+                    value: $(this).text(),
+                    selected: ($(this).text() == defaultValue) ? 'selected' : false
+                }).text( $(this).text() ).appendTo( $(select) );
+            });
+        });
+
+        // Translate select lists and rebuild any jQuery Select2 widgets.
+        pub.waitFor(window, "Weglot", 400, function(){
+            Weglot.on("languageChanged", function(){
+                $('.select-list-options').each(function(){
+                    var select = $(this).parent('.select-list-wrapper').find('select');
+
+                    $(this).find('.w-dyn-item').each(function(i){
+                        console.log(i, $(this).text());
+                        select.find('option').eq(i).text( $(this).text() ).val( $(this).data('lang-en') );
+                    });
+                    if (select.hasClass('select2-hidden-accessible')){
+                        select.select2('destroy');
+                        select.select2( select.data('select2-options') );
+                    }
+                });
+            });
+        });
     });
 
     return pub;
-
 }(jQuery, this, this.document));
 
 
