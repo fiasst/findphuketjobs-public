6490d59e02c7219657ea4db05e59f41aef14196f
Marc Hudson <fiasst@gmail.com>
2023-05-04 03:58:55 +0100

    JS updates

--- js/help.js
+++ js/help.js
@@ -63,66 +63,6 @@ var HELP = (function($, window, document, undefined){
     };
 
 
-    //wait for event to finish, example browser window to stop being resized
-    pub.onEventFinish = (function(callback, ms, uniqueId){
-        var timers = {};
-        
-        return function(callback, ms, uniqueId){
-            if (!uniqueId){
-                uniqueId = "Don't call this twice without a uniqueId";
-            }
-            if (timers[uniqueId]){
-                clearTimeout(timers[uniqueId]);
-            }
-            timers[uniqueId] = setTimeout(callback, ms);
-        };
-    }());
-
-
-    // Add breakpoint helper classes to body on init & window resize
-    pub.bodyBreakpoints = function(){
-        var windowWidth = $(window).width(),
-            bodyClass = false;
-
-        if (windowWidth < HELP.breakpoints.tabletBP){
-            bodyClass = 'bp-mobile';
-        }
-        else {
-            if (windowWidth >= HELP.breakpoints.desktopBP){
-                bodyClass = 'bp-desktop';
-            }
-            else if (windowWidth >= HELP.breakpoints.tabletLandscapeBP){
-                bodyClass = 'bp-tabletLandscape';
-            }
-            else if (windowWidth >= HELP.breakpoints.tabletBP){
-                bodyClass = 'bp-tablet';
-            }
-        }
-        if (bodyClass !== false){
-            $('body').removeClass('bp-mobile bp-tablet bp-tabletLandscape bp-desktop').addClass(bodyClass);
-        }
-    };
-
-
-    // Return a current breakpoint string based on body classes.
-    pub.viewportWidth = function(){
-        var classes = $('body').attr('class');
-
-        if (classes.indexOf('mobile-bp') > -1){
-            return 's';
-        }
-        else if (classes.indexOf('tablet-bp') > -1){
-            return 'm';
-        }
-        else if (classes.indexOf('tabletLandscape-bp') > -1 || classes.indexOf('desktop-bp') > -1){
-            return 'l';
-        }
-        else {
-            return false;
-        }
-    };
-
-
     // Manage cookies.
     pub.setCookie = function(name, value, days){
         var expires = "";
@@ -151,13 +91,7 @@ var HELP = (function($, window, document, undefined){
     
 
     // Init.
-    $(function(){
-        $(window)
-            .on('resize', function(){
-                pub.bodyBreakpoints();
-            })
-            .trigger('resize');
-    });
+    // $(function(){});
 
     
     return pub;
