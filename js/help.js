bb207348481eb9131f6ef373b63fb4aff00a5f8f
Marc Hudson <marc@mustimpress.com>
2023-09-21 05:39:59 +0100

    improve HELP.sanitize() to allow some HTML tags

--- js/help.js
+++ js/help.js
@@ -56,46 +56,43 @@ HELP = (function($, window, document, undefined) {
     // Remove <script> tags and any attributes that start with 'on' (onclick, etc).
         // This helps to guards against XSS attack.
     //
-    /*pub.sanitizeHTML = (str) => {
-        if (!str) return;
-
-        // Remove <script> tags and content.
-        return str.toString()
-            .replace(/<.*?script.*?>/gi, '')
-            .replace(/<script\b[^>]*>(?:[^<]*<\/script>|[^>]*\/>)|<script\b[^>]*\/?>/gi, '')
-            // Remove attributes that start with "on" (eg: "onclick")
-            .replace(/(\s*<[^>]*)(\s+(on\w+)="[^"]*")/gi, '$1')
-            // Remove instances of "javascript:".
-            .replace(/javascript:/gi, '')
-            // Remove instances of "javascript:" in decimal HTML Character.
-            .replace(/&#106;&#97;&#118;&#97;&#115;&#99;&#114;&#105;&#112;&#116;&#58;/gi, '')
-            // Remove instances of "javascript:" in decimal HTML Character (without semicolons).
-            .replace(/&#0000106&#0000097&#0000118&#0000097&#0000115&#0000099&#0000114&#0000105&#0000112&#0000116&#0000058/gi, '');
-    };*/
-    pub.sanitizeHTML = (str) => {
+    pub.sanitizeHTML = (str, allowedTags) => {
         if (!str) return;
 
         const escapeChars = {
             '&': '&amp;', '<': '&lt;', '>': '&gt;'
         };
-        return str.toString()
+        if (allowedTags) {
+            var regex = new RegExp(`<((?!\/?(${allowedTags})\\b)[^>]+)>`, 'gi');
+            // Remove all HTML except a few allowed tags.
+            str = str.replace(regex, '')
+                // Remove "src" from any allowed HTML.
+                .replace(/src/gi, '');
+        }
+        else {
+            // Escape certain HTML characters.
+            // (Match & if not followed by (apos|quot|gt/lt|amp);)
+            str = str.replace(/[<>]|&(?!(?:apos|quot|[gl]t|amp);)/gi, match => escapeChars[match])
+            // All combinations of the character "<" in HTML/JS (semicolon optional):
+                .replace(/(\x3c:?|\u003c:?)|(?:&(amp;)?#0*60;?|&(amp;)?#x0*3c;?):?/gi, '');
+        }
+        str = str.toString()
             // Remove <script> tags and content.
             // Remove ".constructor" to prevent ES6 Set.constructor() from eval() things.
             // Remove "document.cookie" to prevent session hijacking.
             .replace(/<.*?script.*?>|.constructor|document.cookie|document.domain/gi, '')
             // Remove substrings that start with "on" (event attributes. ex: "onclick").
-            .replace(/(\s*<[^>]*)on\w+/gi, '')
-            // Remove instances of "javascript:" or "script:" (for "ascript:").
-            .replace(/javascript.*?:|script.*?:/gi, '')
+            .replace(/<[^>]*\s+[^>]*on\w+[^>]*>/gi, '')
+            // Remove "\\\on" (event attributes that start with a backslash).
+            .replace(/<[^>]*(?:\\{2})*on\w+/gi, '')
+            // Remove instances of "javascript:", "script:" (for "ascript:") or &{ (for "& JS includes").
+            .replace(/javascript.*?:|script.*?:|&{/gi, '')
             // Remove "script:" decimal HTML Characters (&#0000099 or &#99. semicolon optional).
             .replace(/&#0*115;?|&#0*99;?|&#0*114;?|&#0*105;?|&#0*112;?|&#0*116;?|&#0*58;?/g, '')
             // Remove "script:" Hexadecimal HTML Characters (&#x0000073 or &#x73. semicolon optional).
-            .replace(/&#x0*73;?|&#x0*63;?|&#x0*72;?|&#x0*69;?|&#x0*70;?|&#x0*74;?|&#x0*3A;?/gi, '')
-            // Escape certain HTML characters.
-            // (Match & if not followed by (apos|quot|gt/lt|amp);)
-            .replace(/[<>]|&(?!(?:apos|quot|[gl]t|amp);)/gi, match => escapeChars[match])
-            // All combinations of the character "<" in HTML/JS (semicolon optional):
-            .replace(/(\x3c:?|\u003c:?)|(?:&(amp;)?#0*60;?|&(amp;)?#x0*3c;?):?/gi, '')
+            .replace(/&#x0*73;?|&#x0*63;?|&#x0*72;?|&#x0*69;?|&#x0*70;?|&#x0*74;?|&#x0*3A;?/gi, '');
+        
+        return str;
     };
 
 
