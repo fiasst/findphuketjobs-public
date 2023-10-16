45c2cbfe0d1c87bf06c54405f8ccda8c9855a2fb
Marc Hudson <marc@mustimpress.com>
2023-10-16 07:24:35 +0100

    add dropdownParent option to Select2 elements to fix Litbox issues

--- js/vendors/jquery.select2.js
+++ js/vendors/jquery.select2.js
@@ -4622,8 +4622,11 @@ S2.define('select2/dropdown/attachBody',[
       bottom: $window.scrollTop() + $window.height()
     };
 
-    var enoughRoomAbove = viewport.top < (offset.top - dropdown.height);
-    var enoughRoomBelow = viewport.bottom > (offset.bottom + dropdown.height);
+    // @EDIT: by MH.
+    // var enoughRoomAbove = viewport.top < (offset.top - dropdown.height);
+    var enoughRoomAbove = true;
+    // var enoughRoomBelow = viewport.bottom > (offset.bottom + dropdown.height);
+    var enoughRoomBelow = false;
 
     var css = {
       left: offset.left,
